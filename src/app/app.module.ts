66dd643c968b97d78e6f70a63961e89ca86d25c0
Oscar <ommalor@gmail.com>
2019-06-07 10:16:15 -0400

    Added new expenses
    Was added the expeses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule } from '@angular/material';
 import { ExpenseComponent } from './expense/expense.component';
 
 
@@ -27,7 +30,11 @@ import { ExpenseComponent } from './expense/expense.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+
+// Option groups autocomplete
+export interface StateGroup {
+  letter: string;
+  names: string[];
+}
+
+// tslint:disable-next-line:variable-name
+export const _filter = (opt: string[], value: string): string[] => {
+  const filterValue = value.toLowerCase();
+
+  return opt.filter(item => item.toLowerCase().indexOf(filterValue) === 0);
+};
+// End Option groups autocomplete
 
 @Component({
   selector: 'app-expense',
@@ -7,43 +23,137 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
   styleUrls: ['./expense.component.scss']
 })
 export class ExpenseComponent implements OnInit {
+
   // Reactive form
   expenseForm: FormGroup;
+  totalExpended = 0;
+  totalSave = 0;
 
-  constructor() { }
+  // stateForm: FormGroup = this._formBuilder.group({
+  //   stateGroup: '',
+  // });
+
+  stateGroups: StateGroup[] = [{
+    letter: 'C',
+    names: ['Car Loan', 'Car Insurance', 'Car Gas', 'Car Toll', 'Car Parking', 'Car Maintenance', 'Car Appliances', 'Clothing']
+  }, {
+    letter: 'F',
+    names: ['Food']
+  }, {
+    letter: 'G',
+    names: ['Gifts']
+  }, {
+    letter: 'H',
+    names: ['Households', 'Health Care']
+  }, {
+    letter: 'I',
+    names: ['Internet']
+  }, {
+    letter: 'P',
+    names: ['Power & Light', 'Phone Service', 'Phone payments', 'Personal hygiene']
+  }, {
+    letter: 'R',
+    names: ['Restaurant']
+  }, {
+    letter: 'U',
+    names: ['Uber, Lyft']
+  }];
+
+
+  stateGroupOptions: Observable<StateGroup[]>;
+  // tslint:disable-next-line:variable-name
+  // constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.initForm();
+    // tslint:disable-next-line:no-non-null-assertion
+
+    // tslint:disable-next-line:no-non-null-assertion
+    // this.stateGroupOptions = this.expenseForm.get('stateGroup')!.valueChanges
+    //   .pipe(
+    //     startWith(''),
+    //     map(value => this._filterGroup(value))
+    //   );
+  }
+
+  private _filterGroup(value: string): StateGroup[] {
+    if (value) {
+      return this.stateGroups
+        .map(group => ({letter: group.letter, names: _filter(group.names, value)}))
+        .filter(group => group.names.length > 0);
+    }
+
+    return this.stateGroups;
   }
 
   private initForm() {
     const monthlyIncome = '';
     const expensesArray = new FormArray([]);
 
+
     this.expenseForm = new FormGroup({
-      income: new FormControl(monthlyIncome),
+      income: new FormControl(monthlyIncome, [Validators.pattern(/^[1-9]+[0-9]*$/)]),
       expenses: expensesArray
     });
+    this.onAddExpense();
   }
 
+
   onAddExpense() {
-    console.log(this.expenseForm.controls.income.value);
+
     (this.expenseForm.get('expenses') as FormArray).push(
       new FormGroup({
-        name: new FormControl(null, Validators.required),
+        expenseName: new FormControl(''),
+        // name: new FormControl(null, Validators.required),
         amount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
+
     );
+
+    // tslint:disable-next-line:no-non-null-assertion
+    this.stateGroupOptions = ((this.expenseForm.get('expenses') as FormArray).at(0) as FormGroup).controls.expenseName!.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => this._filterGroup(value))
+    );
+
   }
 
   onDeleteExpense(index: number) {
     (this.expenseForm.get('expenses') as FormArray).removeAt(index);
+    this.calculateTotalExpenses();
   }
 
-  onDeleteEmptyRow(index: number) {
+  onNameLeave(index: number) {
     const rowValue = ((this.expenseForm.get('expenses') as FormArray).at(index) as FormGroup).controls;
-    if (rowValue.amount.value == null && rowValue.name.value === '') {
+    if (rowValue.amount.value == null && rowValue.expenseName.value === '') {
       (this.expenseForm.get('expenses') as FormArray).removeAt(index);
+    }
+  }
+
+  onAmountLeave(index: number) {
+    this.onNameLeave(index);
+    this.calculateTotalExpenses();
+  }
+
+  calculateTotalExpenses() {
+// Recalcule the expenses
+    this.totalExpended = 0;
+    for (let i = 0; i < (this.expenseForm.get('expenses') as FormArray).length; i++) {
+      this.totalExpended += ((this.expenseForm.get('expenses') as FormArray).at(i) as FormGroup).controls.amount.value;
+    }
+    this.totalSave = this.expenseForm.value.income - this.totalExpended;
+  }
+
+  onAmountPressEnter(index) {
+// Check if the cursor is in the last Amount and if the form is valid add a new line
+    if ((index === ((this.expenseForm.get('expenses') as FormArray).length) - 1) && this.expenseForm.valid) {
+      (this.expenseForm.get('expenses') as FormArray).push(
+        new FormGroup({
+          expenseName: new FormControl(null, Validators.required),
+          amount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+        })
+      );
     }
   }
 
